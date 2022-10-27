b53ad4d99c0188ec977494f75c35285bf6c569cb
Jakub <jakub.bytow@gmail.com>
2022-10-27 20:45:28 +0200

    v 0.47 orders/cart/accounts WIP

--- Shop/Accounts/views.py
+++ Shop/Accounts/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from .forms import CreateUserForm
 
 
 def register(request):
-    form = UserCreationForm()
+    form = CreateUserForm()
 
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Account was successfully created')
+            return redirect('login')
 
     context = {'form': form}
     return render(request, 'register.html', context)
--- Shop/Shop/urls.py
+++ Shop/Shop/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Products.views import *
-from Account.views import *
+from Accounts.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
