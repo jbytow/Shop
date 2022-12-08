d0cd5767f0299647486ff66a539aa414727f7ffb
Jakub <jakub.bytow@gmail.com>
2022-12-08 21:32:08 +0100

    v 0.62 Orders views/models skeleton updated

--- Shop/Orders/views.py
+++ Shop/Orders/views.py
@@ -6,7 +6,14 @@ from .models import *
 
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderproduct_set.all()
+    else:
+        items = []
+    context = {'items': items}
     return render(request, 'cart.html', context)
 
 
